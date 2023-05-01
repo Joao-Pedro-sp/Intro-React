283f944573d03e1768149889286eeaacac5415a5
joao <jp_santana00@outlook.com>
2023-05-01 12:26:45 -0300

    end note preview

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,21 +1,45 @@
-import { Container, Links } from './styles';
+import { Container, Links, Content } from './styles';
 
 import { Header } from '../../components/Header'
 import { Button } from '../../components/Button'
 import { Section } from '../../components/Section'
+import { Tag } from '../../components/Tag'
+import { ButtonText } from '../../components/ButtonText'
 
 export function Details(){
   return(
     <Container>
       <Header />
 
-      <Section title="Links uteis">
-        <Links>
-          <li><a href="#">https://www.rocketseat.com.br/</a></li>
-          <li><a href="#">https://www.rocketseat.com.br/</a></li>
-        </Links>
-      </Section>
-      <Button title="Voltar" />
+      <main>
+        <Content>
+          <ButtonText title="Excluir nota" />
+
+          <h1>
+            Introducao ao React
+          </h1>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. A ullam pariatur tenetur voluptatum maiores. Fugiat nam blanditiis culpa non? Placeat tenetur magni, natus molestias eos commodi quae debitis aut eius?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. A ullam pariatur tenetur voluptatum maiores. Fugiat nam blanditiis culpa non? Placeat tenetur magni, natus molestias eos commodi quae debitis aut eius?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. A ullam pariatur tenetur voluptatum maiores. Fugiat nam blanditiis culpa non? Placeat tenetur magni, natus molestias eos commodi quae debitis aut eius?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. A ullam pariatur tenetur voluptatum maiores. Fugiat nam blanditiis culpa non? Placeat tenetur magni, natus molestias eos commodi quae debitis aut eius?
+          </p>
+
+          <Section title="Links uteis">
+            <Links>
+              <li><a href="#">https://www.rocketseat.com.br/</a></li>
+              <li><a href="#">https://www.rocketseat.com.br/</a></li>
+            </Links>
+          </Section>
+
+          <Section title="Marcadores">
+            <Tag title="express"/>
+            <Tag title="nodejs"/>
+          </Section>
+          <Button title="Voltar" />
+        </Content>
+      </main>
     </Container>
     
   )
