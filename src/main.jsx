71e567b76352b281bc43c2af61d3166cb1e9ecf7
joao <jp_santana00@outlook.com>
2023-05-08 14:59:10 -0300

    add page signup

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,14 @@ import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './styles/global'
 import theme from './styles/theme';
 
-import { Signin } from './pages/Signin';
+import { SignUp } from './pages/SignUp';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Signin />
+      <SignUp />
     </ThemeProvider>
   </React.StrictMode>,
 )
